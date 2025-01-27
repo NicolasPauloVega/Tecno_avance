c20679220d41de1711aa5cc5a03f7b7f370d73a8
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2025-01-26 22:32:06 -0500

    configuraciones basicas
    Se realizo la configuración básica para la carga de archivos estáticos como JavaScript.

--- Tecno_avance/settings.py
+++ Tecno_avance/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Aplicaciones
+    'user',
+    'admin',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'Tecno_avance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Se añade las rutas de los templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Config static
+STATIC_URL = '/static/' # URL base para archivos estaticos
+STATICFILES_DIRS = [BASE_DIR / 'static'] # Directorio raiz de los archivos estaticos
+
+# Config media (images in database)
+MEDIA_URL = '/media/' # URL base para imagenes
+MEDIA_ROOT = BASE_DIR / 'media' # Directorio raiz de las imagenes
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Tecno_avance/urls.py
+++ Tecno_avance/urls.py
@@ -17,6 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Configuracion archivos media y archivos estaticos (css, js)
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Validacion y carga de URL de archivos estaticos
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
